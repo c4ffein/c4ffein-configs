97d9fa9938d15af2e9803b0009247fdcfff87714
c4ffein <c4ffein>
2020-01-09 02:44:07 +0100

    Added setup_c4ffein_windows.ps1

---
+++ setup_c4ffein_windows.ps1
@@ -0,0 +1,50 @@
+# One script to setup a decent Windows environment from a clean install using Chocolatey, by c4ffein
+
+# Should check certificate
+$certifSubject = 'CN="Chocolatey Software, Inc.", O="Chocolatey Software, Inc.", L=Topeka, S=Kansas, C=US'
+$certifIssuer = 'CN=DigiCert SHA2 Assured ID Code Signing CA, OU=www.digicert.com, O=DigiCert Inc, C=US'
+$certifThumbprint = "4BF7DCBC06F6D0BDFA8A0A78DE0EFB62563C4D87"
+
+
+# Start another PowerShell process as Admin if needed
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList ("-ExecutionPolicy Bypass " + $arguments)
+    Break
+}
+
+
+function FailedIf {
+    If ($args[0]) {
+        Write-Host $args[1] -ForegroundColor Yellow
+        pause
+        exit
+    }
+}
+
+
+Write-Host "Downloading Chocolatey install script" -ForegroundColor Red
+$chocolateyPS = (New-Object System.Net.WebClient).DownloadData('https://chocolatey.org/install.ps1')
+$chocolateyPSString = [System.Text.Encoding]::ASCII.GetString($chocolateyPS)
+# Needed as Get-AuthenticodeSignature can only take Unicode Byte Array as -Content
+$chocolateyPSUnicodeByteArray = [System.Text.Encoding]::Unicode.GetBytes($chocolateyPSString)
+
+
+Write-Host "Checking Chocolatey install script certificate" -ForegroundColor Red
+# TODO : check crypto algorithm
+$certif = (Get-AuthenticodeSignature -Content $chocolateyPSUnicodeByteArray -SourcePathOrExtension "chocolatey_install.ps1").SignerCertificate
+$certif | Format-List
+FailedIf ($certif.Subject -ne $certifSubject) "Certif Subject mismatch. Exiting."
+FailedIf ($certif.Issuer -ne $certifIssuer) "Certif Issuer mismatch. Exiting."
+FailedIf ($certif.Thumbprint -ne $certifThumbprint) "Certif Thumbprint mismatch. Exiting."
+
+
+Write-Host "Executing Chocolatey install script" -ForegroundColor Red
+iex $chocolateyPSString
+
+
+Write-Host "Installing programs" -ForegroundColor Red
+choco install firefox python atom git -y
+
+
+pause
