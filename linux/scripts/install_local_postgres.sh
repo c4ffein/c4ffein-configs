ec4c19f4fa152d041fd829d03210c8d4ad529db7
c4ffein <c4ffein>
2020-03-02 02:56:24 +0100

    install_local_postgres.sh : changed target dir for .local/external

--- linux/scripts/install_local_postgres.sh
+++ linux/scripts/install_local_postgres.sh
@@ -4,10 +4,10 @@
 PGSQL_TARBALL_LINK="https://get.enterprisedb.com/postgresql/postgresql-10.10-1-linux-x64-binaries.tar.gz"
 PGSQL_TARBALL_NAME="postgresql-10.10-1-linux-x64-binaries.tar.gz"
 export PGDATA="$HOME/databases/pgsql_data"
-LOCAL_LIB="$HOME/local"
+LOCAL_STUFF="$HOME/.local/external"
 CHECK_HASH="e67469c797311088aa1e4dbbfaaaeb0017436934664b2899241311a03275d933"
 
-mkdir -p $LOCAL_LIB
+mkdir -p $LOCAL_STUFF
 mkdir -p $PGDATA
 
 wget "$PGSQL_TARBALL_LINK" -O "/tmp/${PGSQL_TARBALL_NAME}"
@@ -16,11 +16,11 @@ if [ "$HASH" != "$CHECK_HASH" ]; then
     echo -e "\e[31mPostgreSQL archive hash not matching. Possible corruption attempt.\e[0m"
     exit
 fi
-tar xv -C "$LOCAL_LIB" -f "/tmp/${PGSQL_TARBALL_NAME}"
+tar x -C "$LOCAL_STUFF" -f "/tmp/${PGSQL_TARBALL_NAME}"
 
-"$LOCAL_LIB/pgsql/bin/initdb"
-# "$LOCAL_LIB/pgsql/bin/pg_ctl" start
-# "$LOCAL_LIB/pgsql/bin/psql" -h localhost postgres # to connect
+"$LOCAL_STUFF/pgsql/bin/initdb"
+# "$LOCAL_STUFF/pgsql/bin/pg_ctl" start
+# "$LOCAL_STUFF/pgsql/bin/psql" -h localhost postgres # to connect
 
 grep -qi PGDATA ~/.bashrc || echo "export PGDATA=$(printf "%q" "$PGDATA")" >> "$HOME/.bashrc"
-grep -qi "$LOCAL_LIB/pgsql" ~/.bashrc || echo "export PATH=$LOCAL_LIB/pgsql/bin:\$PATH" >> "$HOME/.bashrc"
+grep -qi "$LOCAL_STUFF/pgsql" ~/.bashrc || echo "export PATH=$LOCAL_STUFF/pgsql/bin:\$PATH" >> "$HOME/.bashrc"
