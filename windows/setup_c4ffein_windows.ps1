17e43f77fb0a80fe747f85ba553364bff5279fc7
c4ffein <c4ffein>
2020-11-02 22:28:25 +0100

    powertoys for windows

--- windows/setup_c4ffein_windows.ps1
+++ windows/setup_c4ffein_windows.ps1
@@ -45,7 +45,7 @@ iex $chocolateyPSString
 
 
 Write-Host "Installing programs" -ForegroundColor Red
-choco install firefox python atom cmder git gh -y
+choco install firefox python atom cmder git gh powertoys -y
 
 
 Write-Host "Installing Python utility packages" -ForegroundColor Red
