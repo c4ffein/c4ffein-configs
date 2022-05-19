e8f5e61b1472249eb87407ab24ca3519703c79e9
c4ffein <c4ffein>
2022-05-19 17:01:47 +0200

    mutt

---
+++ linux/scripts/install/local/install_local_mutt.py
@@ -0,0 +1,3 @@
+ff8b781159794adcca126b551d5e419174b7b38a582a159bfe4e13451a757e47  mutt-2.2.5.tar.gz
+http://ftp.mutt.org/pub/mutt/mutt-2.2.5.tar.gz
+https://bitbucket.org/mutt/mutt/downloads/mutt-2.2.5.tar.gz
