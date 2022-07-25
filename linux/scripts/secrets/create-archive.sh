84589b3f8ae06190222dcdadb3a6add971e17294
c4ffein <c4ffein>
2022-07-25 20:48:11 +0200

    create-archive in separate dir

--- linux/scripts/secrets/create-archive.sh
+++ linux/scripts/secrets/create-archive.sh
@@ -10,13 +10,15 @@ PASSPHRASE="$SECRETS_PASS"
 TIMESTAMP=$(date +%s)
 FILENAME=c4ffein-secrets-$TIMESTAMP
 cd "$(dirname "$(readlink -f "$0")")"
+mkdir -p "temp"
 mkdir -p "secrets"
-mkdir secrets/"$TIMESTAMP"
-cd secrets/"$TIMESTAMP"
+mkdir temp/"$TIMESTAMP"
+cd temp/"$TIMESTAMP"
 mkdir "$FILENAME"
 cp -r ~/.gnupg "$FILENAME"
 cp -r ~/.ssh "$FILENAME"
 cp -r ~/.local/share/password-store "$FILENAME"  # need gpg key to unlock
 cp ~/.2fa "$FILENAME"  # This is bad, but it depends on the 2fa implem
 tar -czvf "$FILENAME".tar.gz "$FILENAME"
-gpg --pinentry-mode loopback --passphrase "$PASSPHRASE" -c "$FILENAME".tar.gz
+[ -f ../../secrets/"$FILENAME".tar.gz.gpg ] && >&2 echo "$FILENAME".tar.gz.gpg ALREADY EXISTS && exit 7
+gpg  -o ../../secrets/"$FILENAME".tar.gz.gpg  --pinentry-mode loopback --passphrase "$PASSPHRASE" -c "$FILENAME".tar.gz
