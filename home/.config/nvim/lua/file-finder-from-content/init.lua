21f71ed730eee432994b423a89cfdf5022f22afb
c4ffein <c4ffein@gmail.com>
2025-06-11 00:17:33 +0200

    todos

--- home/.config/nvim/lua/file-finder-from-content/init.lua
+++ home/.config/nvim/lua/file-finder-from-content/init.lua
@@ -2,6 +2,10 @@ local M = {}
 
 local files = require("file-finder-from-content.files")
 
+-- TODO shift v to select inner word like v => aw
+-- TODO when using O, selection should be searched for
+-- TODO ctrl j/k to move left/right
+
 function M.setup()
   vim.keymap.set("n", "O", files.find_files, {desc = "Find files", silent = true})
   vim.api.nvim_create_user_command("FfFiles", files.find_files, {desc = "Find files with fuzzy finder"})
