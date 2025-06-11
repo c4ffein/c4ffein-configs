bc88328c7d37cd6308e573ef57f20a06a524f464
c4ffein <c4ffein@gmail.com>
2025-06-11 22:52:21 +0200

    select word

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -66,5 +66,18 @@ vim.cmd('colorscheme c4ffein')
 
 vim.g.editorconfig = false
 
+function select_current_word()
+  local line, cursor = vim.api.nvim_get_current_line(), vim.api.nvim_win_get_cursor(0)
+  local row, w_start, w_end = cursor[1], cursor[2], cursor[2]
+  -- Iterate on current line
+  while w_start  > 0     and    line:sub(w_start,   w_start  ):match('[%w_]') do   w_start = w_start - 1 end
+  if    w_start == 0     or not line:sub(w_start,   w_start  ):match('[%w_]') then w_start = w_start + 1 end
+  while w_end   <= #line and    line:sub(w_end + 1, w_end + 1):match('[%w_]') do   w_end   = w_end   + 1 end
+  -- Actual selection
+  vim.api.nvim_win_set_cursor(0, { row, w_start - 1 }); vim.cmd('normal! v')
+  vim.api.nvim_win_set_cursor(0, { row, ((w_end ~= 0 and w_end) or 1) - 1 })
+end
+vim.keymap.set('n', 'V', select_current_word, { desc = 'select word' })
+
 require('file-finder').setup()
 require('file-finder-from-content').setup()
