9610d5aff26ed463157dd796391dccfec49f9e6c
c4ffein <c4ffein@gmail.com>
2025-06-12 21:18:25 +0200

    better scoring

--- home/.config/nvim/lua/file-finder-from-content/scoring.lua
+++ home/.config/nvim/lua/file-finder-from-content/scoring.lua
@@ -1,36 +1,39 @@
 local M = {}
 
 function M.score(pattern, str, skip_regex_matching)  -- TODO also return pos
-  -- lua pattern matching should be performant enough, comes from user input anyway
+  -- WARNING `find` raises on ["(" => ""] and ["t(" => "tt(("] but doesnt raise on ["t(" => ""]
+  -- no easy and reliable way to do a pre-check, so just update the skip_regex_matching on first fail
   local plain_pos = str:find(pattern, 1, true)  -- start at first char and plain text matching
-  if plain_pos                   then return 3 end
-  if skip_regex_matching         then return 0 end
-  plain_pos = str:find(pattern)  -- now does pattern matching
-  if plain_pos                   then return 1 end
-  return 0
+  if plain_pos                   then return skip_regex_matching, 3 end
+  if skip_regex_matching         then return skip_regex_matching, 0 end
+  local worked, pattern_pos = pcall(string.find, str, pattern)    -- now does pattern matching
+  if not worked                  then return true,                0 end
+  if pattern_pos                 then return skip_regex_matching, 1 end
+  return                                     skip_regex_matching, 0
 end
 
 function M.filter(pattern, items, key_func)
+  -- TODO the state of pattern matching or no should be shown in the ui
   -- TODO handle this before, reuse history like with the o version?
   if not pattern or pattern == "" then return items end
-  -- TODO next line should be shown somewhere in the interface actually? Check before?
-  local do_regex_matching = pcall(string.find, "", pattern)  -- check the status of the protected call
-  local scored_items = {}
+  local scored_items, skip_regex_matching = {}, false
   key_func = key_func or function(item) return item end
   
   for _, item in ipairs(items) do
-    local key = key_func(item)
-    local score = M.score(pattern, item, not do_regex_matching) * 1000
+    local key, item_score, current_score = key_func(item), 0, 0
+    skip_regex_matching, current_score = M.score(pattern, item, skip_regex_matching)
+    item_score = current_score * 1000
     local matched_lines_table = {}
     for line in io.lines(item) do
-      score = score + M.score(pattern, line, not do_regex_matching)
+      skip_regex_matching, current_score = M.score(pattern, line, skip_regex_matching)
+      item_score = item_score + current_score
       -- TODO
       -- if line_score > 0 then
       --   table.insert(matched_lines_table, {item = item, score = score, key = key})
       -- end
     end
-    if score > 0 then
-      table.insert(scored_items, {item = item, score = score, key = key})
+    if item_score > 0 then
+      table.insert(scored_items, {item = item, score = item_score, key = key})
     end
   end
   
