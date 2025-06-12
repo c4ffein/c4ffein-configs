0fe56bc475a7e9aa76289dc9c7796cd73bbb75c6
c4ffein <c4ffein@gmail.com>
2025-06-12 21:23:38 +0200

    rename

--- home/.config/nvim/lua/file-finder-from-content/init.lua
+++ home/.config/nvim/lua/file-finder-from-content/init.lua
@@ -2,11 +2,12 @@ local M = {}
 
 local files = require("file-finder-from-content.files")
 
+
 -- TODO when using O, selection should be searched for
 
 function M.setup()
   vim.keymap.set("n", "O", files.find_files, {desc = "Find files", silent = true})
-  vim.api.nvim_create_user_command("FfFiles", files.find_files, {desc = "Find files with fuzzy finder"})
+  vim.api.nvim_create_user_command("FfFiles", files.find_files, {desc = "Find files with scoring"})
 end
 
 M.find_files = files.find_files
