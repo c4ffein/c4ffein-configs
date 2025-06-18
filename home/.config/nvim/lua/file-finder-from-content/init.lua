269d472cb9d1d5325f77a634efb21f591ad345ba
c4ffein <c4ffein@gmail.com>
2025-06-19 01:09:02 +0200

    todo

--- home/.config/nvim/lua/file-finder-from-content/init.lua
+++ home/.config/nvim/lua/file-finder-from-content/init.lua
@@ -6,7 +6,12 @@ local files = require("file-finder-from-content.files")
 -- TODO when using O, selection should be searched for
 
 function M.setup()
-  vim.keymap.set("n", "O", files.find_files, {desc = "Find files", silent = true})
+  -- TODO o for previous plugin
+  vim.keymap.set("n", "O",     files.find_files, {desc = "Find files", silent = true})
+  vim.keymap.set("n", "<C-o>", files.find_files, {desc = "Find files", silent = true})
+  vim.keymap.set("x", "<C-o>", files.find_files, {desc = "Find files", silent = true})
+  -- TODO C-o in the plugin should switch between 2 modes (o and O)
+  -- TODO command to start the reimplem of the regular file manager
   vim.api.nvim_create_user_command("FfFiles", files.find_files, {desc = "Find files with scoring"})
 end
 
