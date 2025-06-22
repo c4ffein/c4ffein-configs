2436280c1feaa4e8ac4edf468be1330107a39477
c4ffein <c4ffein@gmail.com>
2025-06-22 13:08:40 +0200

    set

--- windows/setup_c4ffein_windows_11.ps1
+++ windows/setup_c4ffein_windows_11.ps1
@@ -1,4 +1,4 @@
-# One script to setup a decent Windows environment from a clean install using Chocolatey, by c4ffein
+# One script to setup a decent Windows environment from a clean install using winget, by c4ffein
 
 winget install -e --id Alacritty.Alacritty
 winget install -e --id=Neovim.Neovim  # TODO : link `vi` to `neovim`
