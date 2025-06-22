9f891faa4ecf2109c52a82f1267a8133b4272782
c4ffein <c4ffein@gmail.com>
2025-06-22 12:56:06 +0200

    path

--- home/.local/env.sh
+++ home/.local/env.sh
@@ -1,3 +1,9 @@
 alias gr=grep
 alias la="ls -lah"
 alias p="python -c"
+
+# Maybe useless past this line, add as see fit
+
+# activate-node-modules() {  # safe with both .bashrc and .zshrc
+#   export PATH="$PATH:$(printf '%q' "$(pwd)")/node_modules/.bin"
+# }
